e6488e6aa49d327a1eb3313c772e7c5ce7f7cadf
Your Name <you@example.com>
2022-02-05 19:58:56 +0300

    first commit

---
+++ HelloWorld.kt
@@ -0,0 +1,9 @@
+/* This is my first  kotlin program
+* here we will showcase basic kotlin syntax
+* like use of comments
+* what fun means
+* what is the work of main function
+* also println*/
+    fun main(){
+        println("hello world")
+    }
